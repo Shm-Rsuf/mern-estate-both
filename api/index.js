9af90cfbcf8f41549f70e48b2bbecaeb7cd429e8
shm_usuf <shmusuf@gmail.com>
2023-10-09 21:59:55 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Our app is running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Our app is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
