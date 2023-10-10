0410b5059aa086e18bd6676d8d1621f701ed7993
shm_usuf <shmusuf@gmail.com>
2023-10-10 19:30:22 +0600

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -11,6 +14,31 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json("user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "user not found"));
+    }
+
+    const validPassword = bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "invalid user or password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
